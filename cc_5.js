4bf229d13e0fe0db6a0a0d14647288aa7483fcd7
H Sandra Ktla <hsandraktla@usf.edu>
2025-02-06 10:29:55 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,11 @@
+//Task 1: Customer Profile - Object Properties
+//Declare an object customer with the properties.
+let customer = {
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@email.com"
+};
+//Log each property using console.log() and a template literal.
+console.log(`Customer Name: ${customer.name}`);
+console.log(`Customer Age: ${customer.age}`);
+console.log(`Customer Email: ${customer.email}`);
